28a99ddc8dfff3c2b40da89a730901b31a033b42
Mark <lopkophacked@protonmail.com>
2024-04-17 13:50:28 +0300

    upd settings

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,7 @@ from dotenv import dotenv_values
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env_vars = dotenv_values(BASE_DIR / 'config/.env')
+env_vars = dotenv_values('/etc/secrets/.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
